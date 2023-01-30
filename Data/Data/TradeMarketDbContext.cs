0263aa2c122faa09a8a6bceb938fbf099c81ffa7
Yuriy Antonov <y.s.antonov.edu@gmail.com>
2023-01-30 13:57:27 +0200

    Initial commit

---
+++ Data/Data/TradeMarketDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Data
+{
+    public class TradeMarketDbContext : DbContext
+    {
+        public TradeMarketDbContext(DbContextOptions<TradeMarketDbContext> options) : base(options)
+        {
+        }
+        //TODO: write DbSets for entities
+
+        //TODO: write Fluent API configuration
+
+    }
+}
---
+++ Data/Data/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Data.Data
+{
+    //TODO: create class UnitOfWork
+}
