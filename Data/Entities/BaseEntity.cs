9b53b0d6eefbb762c255db6c59f471e7c6690817
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-06-13 02:23:47 +0300

    Entities done

---
+++ Data/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities
+{
+    internal class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ Data/Entities/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    internal class ProductCategory : BaseEntity
+    {
+        public string CategoryName { get; set; }
+        
+        public ICollection<Product> Products { get; set; }
+    }
+}
